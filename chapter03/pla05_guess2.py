d6eb4dbc6dfb8b3247d49fd88272bafa01b8a8a9
Oshiro <c24004@std.it-college.ac.jp>
2025-07-10 15:08:53 +0900

    「練習問題：05」を修正

--- chapter03/pla05_guess2.py
+++ chapter03/pla05_guess2.py
@@ -1,6 +1,8 @@
 import random,sys
 num = random.randint(1,10)
-life = 5
+life = 10
+point = 5
+missCount = 0
 print("ゲームを終わるには[0]と入力")
 while life != 0:
     select = int(input("1~10の数字を割り当ててください："))
@@ -9,13 +11,20 @@ while life != 0:
         sys.exit()
     elif select > num:
         print("数字が大きいです")
-        life -= 1
     elif select < num:
         print("数字が小さいです")
-        life -= 1
     else:
         print("当たりです！")
-        life += 10
+        point += 10
         num = random.randint(1,10)
-    print(f"現在のライフ[{life}]")
-print("残念です！")
+        life -= 1
+        missCount = 0
+        print(f"ライフ[{life - missCount}/{life}]:ポイント[{point}]")
+        continue
+    point -= 1
+    missCount += 1
+    print(f"ライフ[{life - missCount}/{life}]:ポイント[{point}]")
+    if missCount == life:
+        print("終了")
+        sys.exit()
+print(f"最終ポイントは[{point}]です")
